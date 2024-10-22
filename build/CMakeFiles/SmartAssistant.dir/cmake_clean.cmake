6215cfbea6c63b6326bdeab4f5fa18089e5cfe5a
McGinnis8 <camcginnis19@gmail.com>
2024-10-22 13:16:11 -0400

    initial commit of whisper

---
+++ build/CMakeFiles/SmartAssistant.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SmartAssistant.dir/main.cpp.o"
+  "CMakeFiles/SmartAssistant.dir/main.cpp.o.d"
+  "CMakeFiles/SmartAssistant.dir/whisper.cpp/examples/common-ggml.cpp.o"
+  "CMakeFiles/SmartAssistant.dir/whisper.cpp/examples/common-ggml.cpp.o.d"
+  "CMakeFiles/SmartAssistant.dir/whisper.cpp/examples/common-sdl.cpp.o"
+  "CMakeFiles/SmartAssistant.dir/whisper.cpp/examples/common-sdl.cpp.o.d"
+  "CMakeFiles/SmartAssistant.dir/whisper.cpp/examples/common.cpp.o"
+  "CMakeFiles/SmartAssistant.dir/whisper.cpp/examples/common.cpp.o.d"
+  "CMakeFiles/SmartAssistant.dir/whisper.cpp/examples/grammar-parser.cpp.o"
+  "CMakeFiles/SmartAssistant.dir/whisper.cpp/examples/grammar-parser.cpp.o.d"
+  "bin/SmartAssistant"
+  "bin/SmartAssistant.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SmartAssistant.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/whisper.cpp/examples/quantize/CMakeFiles/quantize.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Cole/OneDrive/PreviousDocuments/ECE4122/VoiceActivatedSmartAssistant/whisper.cpp/examples/quantize/quantize.cpp" "whisper.cpp/examples/quantize/CMakeFiles/quantize.dir/quantize.cpp.o" "gcc" "whisper.cpp/examples/quantize/CMakeFiles/quantize.dir/quantize.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/Cole/OneDrive/PreviousDocuments/ECE4122/VoiceActivatedSmartAssistant/build/whisper.cpp/examples/CMakeFiles/common.dir/DependInfo.cmake"
+  "/mnt/c/Users/Cole/OneDrive/PreviousDocuments/ECE4122/VoiceActivatedSmartAssistant/build/whisper.cpp/src/CMakeFiles/whisper.dir/DependInfo.cmake"
+  "/mnt/c/Users/Cole/OneDrive/PreviousDocuments/ECE4122/VoiceActivatedSmartAssistant/build/whisper.cpp/ggml/src/CMakeFiles/ggml.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
