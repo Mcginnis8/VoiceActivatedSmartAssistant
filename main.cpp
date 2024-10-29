8eb2bce8b098e876b82cb1570039702c6d691b3c
Snapp, Alexander Björn <akisnapp@gmail.com>
2024-10-29 11:02:50 -0400

    Added support for SFML

--- main.cpp
+++ main.cpp
@@ -7,25 +7,40 @@
 #include <fstream>
 #include <string>
 #include "listener.h"
-#include "functionality.h" // Include the header file
+#include "functionality.h" 
+#include <SFML/Graphics.hpp>
 
 int main(int argc, char **argv) {
+    sf::VideoMode vm(1036, 569);
+    sf::RenderWindow window(vm, "Voice Assistant", sf::Style::Default);
+
     std::cout << "Press 'l' to start listening..." << std::endl;
+    Functionality functionality;
 
-    while (true) {
-        Functionality functionality;
-        char ch = std::cin.get();
-        if (ch == 'l' || ch == 'L') {
-            std::cout << "Listening..." << std::endl;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
+            {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::L) {
+                std::cout << "Listening..." << std::endl;
 
-            // from listener.cpp
-            int predict = Listener::start_main_listen();
-            
-            // from functionality.cpp
-            functionality.executeCommand(predict);
-            
-            std::cout << "Press 'l' to start listening again..." << std::endl;
+                // from listener.cpp
+                int predict = Listener::start_main_listen();
+                
+                // from functionality.cpp
+                functionality.executeCommand(predict);
+                
+                std::cout << "Press 'l' to start listening again..." << std::endl;
+            }
         }
+
+        window.clear();
+
+        window.display();
     }
 
     return 0;
